41cfa6703d75a8ac3f54a3d5a32dfae2fe1c524b
nitesh-20 <nsahu6156@gmail.com>
2024-04-01 17:31:49 +0530

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Mistnav from './Components/Mistnav'
+import Career from './Components/Career'
+import Landscape from './Components/Landscape'
+
+const App = () => {
+  return (
+    <div className="">
+      <Mistnav/>
+      <Career/>
+      <Landscape/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Landscape.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Landscape = () => {
+  return (
+    <div className="Main">
+            <div className="choice font-bold text-4xl flex justify-center py-9 ">
+                Why You can choice midas
+            </div>
+            <div className="content border-2 h-[300] w-full border-blue-900">
+                
+            </div>
+    </div>
+  )
+}
+
+export default Landscape
