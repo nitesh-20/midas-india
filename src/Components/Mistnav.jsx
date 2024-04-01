72a2e2b2b6064816c59b7444f3a40448fd76aa59
nitesh-20 <nsahu6156@gmail.com>
2024-04-01 18:44:37 +0530

    Navbar-done

--- src/Components/Mistnav.jsx
+++ src/Components/Mistnav.jsx
@@ -1,50 +1,173 @@
-import React from 'react'
+
 import mistlogo from '../assets/midas.png'
-const Mistnav = () => {
-    return (
- <div>
-     <div className="navbar bg-white">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+import React, { useState } from "react";
+import { RiMenu2Line } from "react-icons/ri";
+import { TfiClose } from "react-icons/tfi";
+import "./Nevbar.css"
+import mist from "../assets/mist.png"
+
+function Mistnav() {
+  const [NewBar_anima, setNewBar_anima] = useState("");
+  function NewBar_open() {
+    setNewBar_anima("transform translate-x-full");
+  }
+  function NewBar_close() {
+    setNewBar_anima("");
+  }
+
+  return (
+    <>
+      {/* ---------------------Small Devices---------------------- */}
+      <div className="fixed md:hidden w-full flex justify-between px-4 py-2 shadow-2xl items-center bg-white backdrop-blur-[3px] ">
+        <h1 className="text-5xl font-bold text-[#85d88f]">Logo</h1>
+        <div className=" flex gap-4">
+          <button className="bn2 rounded-2xl text-[#ffffff] bg-[#20bb2f]">
+            Sign in !
+          </button>
+          <button
+            onClick={NewBar_open}
+            className=" text-2xl text-[#20bb2f] p-2 border-2 border-[#20bb2f] rounded-2xl"
+          >
+            <RiMenu2Line />
+          </button>
+        </div>
       </div>
-      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
-      </ul>
-    </div>
-    <a className="btn btn-ghost text-xl "><img src={mistlogo}  className="text h-12" alt="" /></a>
-  </div>
-  <div className="navbar-center hidden lg:flex text-green font-bold">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>About-Us</a></li>
-      <li>
-        <details>
-          <summary>Courses</summary>
-          <ul className="p-2">
-            <li><a>Full stack</a></li>
-            <li><a>Data science</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Contact</a></li>
-      <li><a>Placement</a></li>
-    </ul>
-  </div>
-  <div className="navbar-end">
-          <input className='search h-10 rounded-lg' type="text"  placeholder="Search" />
-  </div>
-</div>
- </div>
-  )
+
+      <div
+        className={` fixed flex bg-[#2f2f3048] w-full h-screen top-0 left-[-100%] ${NewBar_anima} transition-all duration-700 ease-in-out`}
+      >
+        <div className="w-4/5 bg-white h-screen">
+          {/* --------------- */}
+
+          <div className=" border-2 h-28">
+            <h1 className="font-bold text-5xl p-2 text-[#20bb2f]">Logo</h1>
+
+            <div className="relative left-40">
+              <button className="text-xl">
+                Sign in <i className="fa-regular fa-user"></i>
+              </button>
+            </div>
+          </div>
+
+          <div>
+            <ul>
+              <li>
+                <a
+                  href="#"
+                  data-link-alt="Home"
+                  className="text_hover relative block p-4 text-xl overflow-hidden"
+                >
+                  <span>Home</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  data-link-alt="Courses"
+                  className="text_hover relative block p-4 text-xl overflow-hidden"
+                >
+                  <span>Courses</span>
+                </a>
+              </li>
+        
+              <li>
+                <a
+                  href="#"
+                  data-link-alt="About"
+                  className="text_hover relative block p-4 text-xl overflow-hidden"
+                >
+                  <span>About</span>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  data-link-alt="Contact"
+                  className="text_hover relative block p-4 text-xl overflow-hidden"
+                >
+                  <span>Contact</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          {/* -------------------*/}
+        </div>
+        <div
+          onClick={NewBar_close}
+          className=" bg-[rgba(0,0,0,0.61)] w-1/4 h-screen "
+        >
+          <h1 className="text-white text-xl p-8">
+            <TfiClose />
+          </h1>
+        </div>
+      </div>
+      {/* ---------------------Small Devices---------------------- */}
+
+      {/* ----------------------Medium devices--------------------------- */}
+
+      <div
+        className={`hidden md:flex fixed w-full border-solid h-16 bg-transparent  backdrop-blur-[3px]`}
+      >
+        <div className=" shadow-2xl bg-[#ffffff] w-full bg-transparent flex justify-center ">
+          <div className="Nav_open_container flex items-center justify-between h-full w-10/12">
+            <div className="Logo h-full">
+              <h1 className="font-bold text-5xl text-[#20bb2f]">Logo</h1>
+            </div>
+            <div className="options flex items-center h-full">
+              <ul className="flex">
+                <li className="">
+                  <a
+                    href="#"
+                    data-link-alt="Home"
+                    className="text_hover relative block p-4 text-xl overflow-hidden"
+                  >
+                    <span>Home</span>
+                  </a>
+                </li>
+                <li className="sm:bg-transparent">
+                  <a
+                    href="#"
+                    data-link-alt="Courses"
+                    className="text_hover relative block p-4 text-xl overflow-hidden"
+                  >
+                    <span>Courses</span>
+                  </a>
+                </li>
+                <li className="">
+                  <a
+                    href="#"
+                    data-link-alt="About"
+                    className=" text_hover relative block overflow-hidden p-4 text-xl "
+                  >
+                    <span>About</span>
+                  </a>
+                </li>
+                <li className="">
+                  <a
+                    href="#"
+                    data-link-alt="Contact"
+                    className="text_hover relative block overflow-hidden p-4 text-xl "
+                  >
+                    <span>Contact</span>
+                  </a>
+                </li>
+              </ul>
+
+              <div className="Register ml-10">
+                <a href="#">
+                  <button className="bn1 rounded-md text-[#7dc116]">
+                    Register !
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* ----------------------Medium devices--------------------------- */}
+    </>
+  );
 }
 
 export default Mistnav;
